9ba19d57d4adadfab7a66ff012e34bd10090a675
mwila <mpnyirongo@gmail.com>
2023-04-04 11:26:52 +0200

    changed from SA to Super Administrator

--- src/app/auth/authRoles.js
+++ src/app/auth/authRoles.js
@@ -1,10 +1,10 @@
 export const authRoles = {
-    sa: ['SA'], // Only SA has access
-    hq: ['SA', 'Headquarters Administrator'], // Only SA has access
-    district: ['SA', 'Headquarters Administrator', 'District Administrator'], // Only SA & Administrator has access
-    school: ['SA', 'Headquarters Administrator', 'District Administrator', 'School Administrator'], // Only SA & Administrator & Editor has access
-    teacher: ['SA', 'Headquarters Administrator', 'District Administrator', 'School Administrator', 'Teacher'], // Only SA & Administrator & Editor has access
-    parent: ['SA', 'Headquarters Administrator', 'District Administrator', 'School Administrator', 'Teacher', 'Parent'], // Only SA & Administrator & Editor has access
-    student: ['SA', 'Headquarters Administrator', 'District Administrator', 'School Administrator', 'Teacher', 'Student'], // Only SA & Administrator & Editor has access
-    guest: ['SA', 'Headquarters Administrator', 'District Administrator', 'School Administrator', 'Teacher', 'Parent','Student', 'Guest'] // Only SA & Administrator & Editor has access
+    sa: ['Super Administrator'], // Only Super Administrator has access
+    hq: ['Super Administrator', 'Headquarters Administrator'], // Only Super Administrator has access
+    district: ['Super Administrator', 'Headquarters Administrator', 'District Administrator'], // Only Super Administrator & Administrator has access
+    school: ['Super Administrator', 'Headquarters Administrator', 'District Administrator', 'School Administrator'], // Only Super Administrator & Administrator & Editor has access
+    teacher: ['Super Administrator', 'Headquarters Administrator', 'District Administrator', 'School Administrator', 'Teacher'], // Only Super Administrator & Administrator & Editor has access
+    parent: ['Super Administrator', 'Headquarters Administrator', 'District Administrator', 'School Administrator', 'Teacher', 'Parent'], // Only Super Administrator & Administrator & Editor has access
+    student: ['Super Administrator', 'Headquarters Administrator', 'District Administrator', 'School Administrator', 'Teacher', 'Student'], // Only Super Administrator & Administrator & Editor has access
+    guest: ['Super Administrator', 'Headquarters Administrator', 'District Administrator', 'School Administrator', 'Teacher', 'Parent','Student', 'Guest'] // Only Super Administrator & Administrator & Editor has access
 }
